d0908100b646f96cff96bdd756796237a67b1f4e
Octavio Martinez <omartinez@logoscorp.com>
2022-04-02 07:10:11 -0400

    data-app

--- class_wp_appointment.php
+++ class_wp_appointment.php
@@ -46,7 +46,7 @@ class WP_Appointment
         <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
-        <div id="app">
+        <div id="app" data-app>
             <v-row>
                 <v-col>
                     <v-select label="Especialidad" :items="especialidades"></v-select>
