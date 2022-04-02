9c0f99f7dcbd7feaabf69f05a2d167317907c78d
Octavio Martinez <omartinez@logoscorp.com>
2022-04-01 21:08:48 -0400

    incluir vue

--- class_wp_appointment.php
+++ class_wp_appointment.php
@@ -12,18 +12,28 @@ class WP_Appointment
 
     public static function init()
     {
-        add_shortcode( 'calendar', array('WP_Appointment', 'render_calendar') );
+        add_shortcode('calendar', array('WP_Appointment', 'render_calendar'));
+    }
+
+    public static function get_dominio()
+    {
+        if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on') {
+            return "http://" . $_SERVER['SERVER_NAME'];
+        } else {
+            return "https://" . $_SERVER['SERVER_NAME'];
+        }
     }
 
 
-    public static function insert_in_head(){
+    public static function insert_in_head()
+    {
         ob_start();
-        ?>
-            <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
-            <link href="https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css" rel="stylesheet">
-            <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
+?>
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
 
-<?php
+    <?php
         $html = ob_get_contents();
         ob_end_clean();
         return $html;
@@ -32,26 +42,21 @@ class WP_Appointment
     public static function render_calendar()
     {
         ob_start();
-?>
-<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
-            <link href="https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css" rel="stylesheet">
-            <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
-        <div id="app">           
-             <v-calendar
-                ref="calendar"
-                v-model="value"
-                :weekdays="weekday"
-                :type="type"
-                :events="events"
-                :event-overlap-mode="mode"
-                :event-overlap-threshold="30"
-            >
-            </v-calendar> 
+    ?>
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
+        <div id="app">
+            <v-calendar ref="calendar" v-model="value" :weekdays="weekday" :type="type" :events="events" :event-overlap-mode="mode" :event-overlap-threshold="30">
+            </v-calendar>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
-  
-        <script src="wp-content/plugins/wp_appointment/App.js"></script> 
+        <script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
+
+        <script src="<?= self::get_dominio() ?>/wp-content/plugins/wp_appointment/app.js"></script>
+        <script>
+            <?php include 'app.js'; ?>
+        </script>
 <?php
         $html = ob_get_contents();
         ob_end_clean();
