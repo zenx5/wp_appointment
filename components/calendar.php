3d6c83d668e30e669204ed96cc983871868f8b6e
Octavio Martinez <omartinez@logoscorp.com>
2022-04-04 05:58:16 -0400

    arg ajax

--- components/calendar.php
+++ components/calendar.php
@@ -2,7 +2,7 @@
 <link href="https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
 <code>
-    <?= json_encode(self::ajax_data()) ?>
+    <?= json_encode(self::ajax_data('appointments')) ?>
 </code>
 <div id="app" data-app>
     <v-row>
